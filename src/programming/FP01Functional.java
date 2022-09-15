188b3d40ac495e695713f30c81dda481664b0d3f
Maru Sosa <ms@wayakna.com>
2022-09-15 14:28:24 -0500

    more excercises

--- src/programming/FP01Functional.java
+++ src/programming/FP01Functional.java
@@ -6,7 +6,13 @@ public class FP01Functional {
     public static void main(String[] args) {
         List<Integer> numbers = List.of(12,1,13,4,6,2,4,12,20);
         //printAllNumbersInListFunctional(numbers);
-        printEvenNumbersInListFunctional(numbers);
+        //printEvenNumbersInListFunctional(numbers);
+
+        List<String> courses = List.of("Spring", "Spring Boot", "API", "Microservices",
+                "AWS", "PCF", "Azure", "Docker", "Kubernetes");
+        //printAllCoursesIndividually(courses);
+        //printSpringCourses(courses);
+        printCourseWithAtLeast4Letters(courses);
     }
 
     /*private static void print(int number) {
@@ -32,6 +38,23 @@ public class FP01Functional {
         //lambda expression = method with simpler syntax of defining a method
         numbers.stream()
                 .filter(number -> number%2 == 0)
+                .forEach(System.out::println);
+    }
+
+    private static void printAllCoursesIndividually(List<String> courses) {
+        courses.stream()
+                .forEach(System.out::println);
+    }
+
+    private static void printSpringCourses(List<String> courses) {
+        courses.stream()
+                .filter(course -> course.contains("Spring"))
+                .forEach(System.out::println);
+    }
+
+    private static void printCourseWithAtLeast4Letters(List<String> courses) {
+        courses.stream()
+                .filter(course -> course.length() > 4)
                 .forEach(System.out::println);
     }
 }
